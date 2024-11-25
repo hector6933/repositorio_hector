67f52645b07523bec546566beb72c021d378fd74
hector6933 <hcriadog@iesch.org>
2024-11-25 10:13:28 +0100

    Cosas de funciones to guapas

---
+++ AplicacionesWeb/JavaScript/Funciones/js/funciones.js
@@ -0,0 +1,34 @@
+let num1 = parseInt(prompt("Dame el primer número:"));
+let num2 = parseInt(prompt("Dame el segundo número:"))
+
+function sumar(num1, num2) {
+
+    let resultado = num1 + num2;
+
+    alert("La suma es: " + resultado);
+
+}
+
+function restar(num1, num2) {
+
+    let resultado = num1 - num2;
+
+    alert("La resta es: " + resultado);
+
+}
+
+function multiplicar(num1, num2) {
+
+    let resultado = num1 * num2;
+
+    alert("La multiplicación es: " + resultado);
+
+}
+
+function dividir(num1, num2) {
+
+    let resultado = num1 / num2;
+
+    alert("La división es: " + resultado);
+
+}
