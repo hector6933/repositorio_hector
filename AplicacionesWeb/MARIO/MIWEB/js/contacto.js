f1cdf69de6ac9a8fa2db709ec5081c043412aefe
hector6933 <hcriadog@iesch.org>
2025-01-20 10:21:12 +0100

    Página de contacto

---
+++ AplicacionesWeb/MARIO/MIWEB/js/contacto.js
@@ -0,0 +1,20 @@
+function success() {
+
+    let etiqueta = document.getElementById("alertaBien");
+
+if (etiqueta.classList.contains("d-none")) {
+  etiqueta.classList.remove("d-none");
+}
+    
+
+}
+
+function mal () {
+
+    let etiqueta = document.getElementById("alertaMal");
+
+if (etiqueta.classList.contains("d-none")) {
+  etiqueta.classList.remove("d-none");
+}
+
+}
